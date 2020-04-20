26cb0a7c669b24bf8b607ff7c0acbdf1fb7bf4cc
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-04-20 14:25:32 +0200

    Initial commit

---
+++ src/main/java/info/unterrainer/commons/rdbutils/Transactions.java
@@ -0,0 +1,9 @@
+package info.unterrainer.commons.rdbutils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Transactions {
+
+}
---
+++ src/test/java/info/unterrainer/commons/rdbutils/TransactionalTests.java
@@ -0,0 +1,13 @@
+package info.unterrainer.commons.rdbutils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TransactionalTests {
+
+	@Test
+	public void works() {
+		assertThat(true).isTrue();
+	}
+}
