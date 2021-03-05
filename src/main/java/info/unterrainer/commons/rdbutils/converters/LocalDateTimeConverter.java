62a565b8088d5ce48f1642f1db4349e5add7c0e5
Gerald Unterrainer <gu_falcon@hotmail.com>
2021-03-05 09:13:39 +0100

    repair ZonedDateTime as well
    some refactoring

--- src/main/java/info/unterrainer/commons/rdbutils/converters/LocalDateTimeConverter.java
+++ src/main/java/info/unterrainer/commons/rdbutils/converters/LocalDateTimeConverter.java
@@ -12,16 +12,14 @@ import info.unterrainer.commons.jreutils.DateUtils;
 @Converter()
 public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
-	DateUtils u;
-
 	@Override
-	public Timestamp convertToDatabaseColumn(final LocalDateTime attribute) {
-		return attribute == null ? null
-				: new Timestamp(DateUtils.utcLocalDateTimeToEpoch(attribute.truncatedTo(ChronoUnit.MICROS)));
+	public Timestamp convertToDatabaseColumn(final LocalDateTime entityValue) {
+		return entityValue == null ? null
+				: new Timestamp(DateUtils.utcLocalDateTimeToEpoch(entityValue.truncatedTo(ChronoUnit.MICROS)));
 	}
 
 	@Override
-	public LocalDateTime convertToEntityAttribute(final Timestamp dbData) {
-		return dbData == null ? null : DateUtils.epochToUtcLocalDateTime(dbData.getTime());
+	public LocalDateTime convertToEntityAttribute(final Timestamp dbValue) {
+		return dbValue == null ? null : DateUtils.epochToUtcLocalDateTime(dbValue.getTime());
 	}
 }
