ef077e86fadd821c75daf5520056a59fe0dda741
Gerald Unterrainer <gu_falcon@hotmail.com>
2021-03-05 09:10:28 +0100

    repair LocalDateTime conversion functions

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-12-10T07:24:25Z";
-	public static final String pomVersion = "0.1.2";
+	public static final String buildTime = "2021-03-05T08:10:04Z";
+	public static final String pomVersion = "0.1.3";
 }
--- src/main/java/info/unterrainer/commons/rdbutils/converters/LocalDateTimeConverter.java
+++ src/main/java/info/unterrainer/commons/rdbutils/converters/LocalDateTimeConverter.java
@@ -7,16 +7,21 @@ import java.time.temporal.ChronoUnit;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+import info.unterrainer.commons.jreutils.DateUtils;
+
 @Converter()
 public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
+	DateUtils u;
+
 	@Override
 	public Timestamp convertToDatabaseColumn(final LocalDateTime attribute) {
-		return attribute == null ? null : Timestamp.valueOf(attribute.truncatedTo(ChronoUnit.MICROS));
+		return attribute == null ? null
+				: new Timestamp(DateUtils.utcLocalDateTimeToEpoch(attribute.truncatedTo(ChronoUnit.MICROS)));
 	}
 
 	@Override
 	public LocalDateTime convertToEntityAttribute(final Timestamp dbData) {
-		return dbData == null ? null : dbData.toLocalDateTime();
+		return dbData == null ? null : DateUtils.epochToUtcLocalDateTime(dbData.getTime());
 	}
 }
