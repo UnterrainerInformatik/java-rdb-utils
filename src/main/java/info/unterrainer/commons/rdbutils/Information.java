df1a449a775f858bc299f6fded5af260746e9794
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2022-03-14 15:36:10 +0100

    update liquibase and hibernate core

--- src/main/java/info/unterrainer/commons/rdbutils/Information.java
+++ src/main/java/info/unterrainer/commons/rdbutils/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.rdbutils;
 
 public class Information {
 	public static final String name = "RDB-Utils";
-	public static final String buildTime = "2022-03-14T14:27:25Z";
+	public static final String buildTime = "2022-03-14T14:34:56Z";
 	public static final String pomVersion = "0.1.15";
 }
