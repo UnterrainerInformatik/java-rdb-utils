98aa45d23f08c72f48bf2a8b42a509fad7b5c3f0
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-06-26 11:19:50 +0200

    update pom (update again before master-push) and .gitignore

--- src/main/java/info/unterrainer/commons/rdbutils/Information.java
+++
@@ -1,7 +0,0 @@
-package info.unterrainer.commons.rdbutils;
-
-public class Information {
-	public static final String name = "RDB-Utils";
-	public static final String buildTime = "2022-06-04T22:27:29Z";
-	public static final String pomVersion = "0.1.18";
-}
